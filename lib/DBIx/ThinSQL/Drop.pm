07f5ae4cf8d28fadaf0d8d91b32965d7075c1d29
Mark Lawrence <nomad@null.net>
2013-12-13 12:59:36 +0100

    VERSION 0.0.9_2

--- lib/DBIx/ThinSQL/Drop.pm
+++ lib/DBIx/ThinSQL/Drop.pm
@@ -5,7 +5,7 @@ use File::ShareDir qw/dist_dir/;
 use Path::Tiny;
 use DBIx::ThinSQL::Deploy;
 
-our $VERSION = '0.0.9_1';
+our $VERSION = '0.0.9';
 
 sub drop_everything {
     my $self      = shift;
