c3851294877bd579582ccbaece4b23da42c201bf
Mark Lawrence <nomad@null.net>
2014-04-10 15:51:31 +0200

    drop_tables() also needs to drop indexes & triggers

--- share/Drop/SQLite/tables.sql
+++ share/Drop/SQLite/tables.sql
@@ -1,6 +1,8 @@
 PRAGMA writable_schema = 1;
 
 delete from sqlite_master where type = 'table';
+delete from sqlite_master where type = 'index';
+delete from sqlite_master where type = 'trigger';
 
 PRAGMA writable_schema = 0;
 
