292ec0e31b133ccb1d544b0aeb6523cde2f1ce2e
Mark Lawrence <nomad@null.net>
2015-06-19 03:24:09 +0200

    Start development on version 0.0.37_1

--- lib/DBIx/ThinSQL.pm
+++ lib/DBIx/ThinSQL.pm
@@ -70,7 +70,7 @@ use Exporter::Tidy
   };
 
 our @ISA     = 'DBI';
-our $VERSION = '0.0.36';
+our $VERSION = '0.0.37_1';
 
 sub _ejoin {
     my $joiner = shift;
--- lib/DBIx/ThinSQL/Drop.pm
+++ lib/DBIx/ThinSQL/Drop.pm
@@ -5,7 +5,7 @@ use File::ShareDir qw/dist_dir/;
 use Path::Tiny;
 use DBIx::ThinSQL::Deploy;
 
-our $VERSION = '0.0.36';
+our $VERSION = '0.0.37_1';
 
 sub _doit {
     my $self   = shift;
