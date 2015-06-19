f28ee1e5594e0b36f3b3e91ecf313304bb5dd8d4
Mark Lawrence <nomad@null.net>
2015-06-19 03:23:57 +0200

    Set version to 0.0.36

--- lib/DBIx/ThinSQL.pm
+++ lib/DBIx/ThinSQL.pm
@@ -70,7 +70,7 @@ use Exporter::Tidy
   };
 
 our @ISA     = 'DBI';
-our $VERSION = '0.0.35_2';
+our $VERSION = '0.0.36';
 
 sub _ejoin {
     my $joiner = shift;
--- lib/DBIx/ThinSQL/Deploy.pm
+++ lib/DBIx/ThinSQL/Deploy.pm
@@ -5,7 +5,7 @@ use Log::Any qw/$log/;
 use Carp qw/croak carp confess/;
 use Path::Tiny;
 
-our $VERSION = '0.0.35_2';
+our $VERSION = '0.0.36';
 
 sub _split_sql {
     my $input = shift;
