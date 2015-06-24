c3584687140112256008094953bc9fe32bf2c4b1
Mark Lawrence <nomad@null.net>
2015-06-24 22:15:47 +0200

    Set version to 0.0.38

--- lib/DBIx/ThinSQL.pm
+++ lib/DBIx/ThinSQL.pm
@@ -70,7 +70,7 @@ use Exporter::Tidy
   };
 
 our @ISA     = 'DBI';
-our $VERSION = '0.0.37_1';
+our $VERSION = '0.0.38';
 
 sub _ejoin {
     my $joiner = shift;
--- lib/DBIx/ThinSQL/Drop.pm
+++ lib/DBIx/ThinSQL/Drop.pm
@@ -5,7 +5,7 @@ use File::ShareDir qw/dist_dir/;
 use Path::Tiny;
 use DBIx::ThinSQL::Deploy;
 
-our $VERSION = '0.0.37_1';
+our $VERSION = '0.0.38';
 
 sub _doit {
     my $self   = shift;
