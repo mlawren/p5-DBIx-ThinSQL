e2fa2efd229ed428879341831dce80a1f8d69736
Mark Lawrence <nomad@null.net>
2016-05-20 23:16:12 +0200

    Set version to 0.0.44

--- lib/DBIx/ThinSQL.pm
+++ lib/DBIx/ThinSQL.pm
@@ -27,7 +27,7 @@ use Exporter::Tidy
   ];
 
 our @ISA     = 'DBI';
-our $VERSION = '0.0.43_1';
+our $VERSION = '0.0.44';
 
 sub ejoin {
     my $joiner = shift;
@@ -876,7 +876,7 @@ DBIx::ThinSQL - A lightweight SQL helper for DBI
 
 =head1 VERSION
 
-0.0.43_1 (yyyy-mm-dd) development release.
+0.0.44 (yyyy-mm-dd) development release.
 
 =head1 SYNOPSIS
 
--- lib/DBIx/ThinSQL/Drop.pm
+++ lib/DBIx/ThinSQL/Drop.pm
@@ -5,7 +5,7 @@ use File::ShareDir qw/dist_dir/;
 use Path::Tiny;
 use DBIx::ThinSQL::Deploy;
 
-our $VERSION = '0.0.43_1';
+our $VERSION = '0.0.44';
 
 sub _doit {
     my $self   = shift;
